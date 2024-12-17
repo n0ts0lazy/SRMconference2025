86df77c2db9a71721c05ba5559268cb759d25a3b
Arnab <55456139+n0ts0lazy@users.noreply.github.com>
2024-12-18 03:46:52 +0530

    page framework core

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Project README 
+
+link to pages: https://n0ts0lazy.github.io/SRMconference2025/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-// JavaScript file 
+document.addEventListener("DOMContentLoaded", function () {
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (prefersDark) {
+        console.log("Dark mode is active.");
+    } else {
+        console.log("Light mode is active.");
+    }
+});
